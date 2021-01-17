d2f68828930e2d7d9dad320350c9f42647b24b5b
soniascr <soniascr@gmail.com>
2021-01-17 12:05:45 +0000

    working todo list, missing some features

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,95 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Todo({ todo, index, removeTodo }) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo">
+			{todo.text}
+			<div>
+				<button className="close" onClick={() => removeTodo(index)}>
+					x
+				</button>
+			</div>
 		</div>
 	);
 }
+
+export function TodoForm({ addTodo }) {
+	const [value, setValue] = React.useState("");
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		setValue("");
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				className="input"
+				value={value}
+				placeholder={"What needs to be done?"}
+				onChange={e => setValue(e.target.value)}
+			/>
+		</form>
+	);
+}
+
+export function Home() {
+	const [todos, setTodos] = React.useState([
+		{ text: "Make the bed" },
+		{ text: "Wash my hands" },
+		{ text: "Build really cool todo app" }
+	]);
+
+	const addTodo = text => {
+		const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+
+	const removeTodo = index => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="container text-center">
+			<h1 className="display-4">todos</h1>
+			<div className="row">
+				<div className="col-lg-4" />
+				<div className="col-lg-4">
+					<ul className="list-group m-3">
+						<li className="list-group-item p-2">
+							<TodoForm addTodo={addTodo} />
+						</li>
+						<li className="list-group-item p-2">
+							{todos.map((todo, index) => (
+								<Todo
+									key={index}
+									index={index}
+									todo={todo}
+									removeTodo={removeTodo}
+								/>
+							))}
+						</li>
+					</ul>
+				</div>
+				<div className="col-lg-4" />
+			</div>
+		</div>
+	);
+}
+
+Todo.propTypes = {
+	todo: PropTypes.object,
+	index: PropTypes.string,
+	removeTodo: PropTypes.function
+};
+
+TodoForm.propTypes = {
+	addTodo: PropTypes.function
+};
